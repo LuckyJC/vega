dee0d3b4f536e75c5380297faa55f2388c90a2b0
LuckyJC <github@jimmycarson.com>
2017-05-20 14:00:33 -0400

    apply constraints to database tables using data annotations

--- Models/Make.cs
+++ Models/Make.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace vega.Models
 {
@@ -7,6 +8,8 @@ namespace vega.Models
     {
         public int Id { get; set; }
 
+        [Required]
+        [StringLength(255)]
         public string Name { get; set; }
 
         //could also use list instead of collection if wanted; list would allow us to use index
