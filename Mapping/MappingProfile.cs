073887fe97438571eb418eb619174257098b7ab2
LuckyJC <github@jimmycarson.com>
2017-06-01 10:08:47 -0400

    refactor using LINQ

--- Mapping/MappingProfile.cs
+++ Mapping/MappingProfile.cs
@@ -26,21 +26,16 @@ namespace vega.Mapping
                 .ForMember(v => v.ContactEmail, opt => opt.MapFrom(vr => vr.Contact.Email))
                 .ForMember(v => v.ContactPhone, opt => opt.MapFrom(vr => vr.Contact.Phone))
                 .ForMember(v => v.Features, opt => opt.Ignore())
-                //ignoring the mapping of features because we need to do some processing
                 .AfterMap((vr, v) => {
                     //remove unselected features
-                    var removedFeatures = new List<VehicleFeature>(); //need to create list to store removed features; can't remove directly or you get runtime error
-                    foreach(var f in v.Features)
-                        if(!vr.Features.Contains(f.FeatureId))
-                            removedFeatures.Add(f);
-                    //iterate over removed features list and remove from Features collection
+                    var removedFeatures = v.Features.Where(f => !vr.Features.Contains(f.FeatureId));
                     foreach(var f in removedFeatures)
                         v.Features.Remove(f);
 
                     //add new features
-                    foreach(var id in vr.Features)
-                        if(!v.Features.Any(f => f.FeatureId == id))  //if we don't have a feature with this identifier, add to collection
-                            v.Features.Add(new VehicleFeature { FeatureId = id})  //initialize immediately
+                    var addedFeatures = vr.Features.Where(id => !v.Features.Any(f => f.FeatureId == id)).Select(id => new VehicleFeature {FeatureId = id});
+                    foreach(var f in addedFeatures)
+                        v.Features.Add(f);
                 });
                 
          }
